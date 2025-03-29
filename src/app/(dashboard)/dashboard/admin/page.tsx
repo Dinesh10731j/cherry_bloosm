0e5c9c058f7a10ebb9a658e9297412fb211cdf14
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-03-29 11:52:28 +0545

    Refactor admin dashboard: remove unused components, enhance sidebar functionality, and improve layout responsiveness

--- src/app/(dashboard)/dashboard/admin/page.tsx
+++ src/app/(dashboard)/dashboard/admin/page.tsx
@@ -1,15 +1,15 @@
 "use client"
 import React from "react";
-import DashBoardLayout from "../layout";
-
 const AdminDashboard = () => {
   return (
     <>
 
-    <DashBoardLayout role ='admin'>
-    <h1>Admin Dashboard</h1>
+
+<h1>Admin Dashboard</h1>
     <p>Welcome to the admin dashboard</p>
-    </DashBoardLayout>
+  
+
+
       
     </>
    
--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,12 +1,10 @@
 import Sidebar from "@/components/sidebar";
-interface LayoutProps {
-  children: React.ReactNode;
-  role: "admin" | "staff" | "customer";
-}
+import { LayoutProps } from "@/lib/utils";
 
 const DashBoardLayout: React.FC<LayoutProps> = ({ children, role }) => {
+  console.log("The role is ",role)
   return (
-    <div className="flex">
+    <div className="flex gap-2.5">
       <Sidebar role={role} />
       <main className="flex-1">{children}</main>
     </div>
